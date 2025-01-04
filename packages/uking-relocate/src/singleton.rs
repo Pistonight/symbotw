c6c68cf4ea4ec4d7412bfedc8d190aea9a7d1af8
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2025-01-03 20:56:00 -0800

    update singleton types

--- packages/uking-relocate/src/singleton.rs
+++ packages/uking-relocate/src/singleton.rs
@@ -1,20 +1,30 @@
-use uking_relocate_lib::{Env, Singleton, SingletonAlloc};
+use uking_relocate_lib::singleton::{CreateByteCode as C, Singleton, SingletonInfo};
+use uking_relocate_lib::Env;
 
 #[allow(dead_code)]
-pub fn pause_menu_data_mgr(env: Env) -> SingletonAlloc {
-    let size = 0x44808;
-    let (create, ctor_invoke) = if env.is_1_6_0() {
+pub fn pause_menu_data_mgr(env: Env) -> SingletonInfo {
+    let rel_start = 0xaaaaaaa0; // TODO, based on env
+    let size = 0x44808; // should be the same for all envs
+                        //
+    let create_bytecode = if env.is_1_6_0() {
         todo!()
     } else {
-        ((0x0096b1cc, None), 0x0096b23c)
+        vec![
+            C::Enter(0x0096b1cc),
+            C::ExecuteUntil(0x0096b200),
+            C::Allocate,
+            C::Jump(0x0096b204),
+            C::ExecuteUntil(0x0096b218),
+            // skip the Disposer ctor
+            C::Jump(0x0096b21c),
+            C::ExecuteToReturn,
+        ]
     };
-    let rel_start = 0xaaaaaaa0; // TODO
 
-    SingletonAlloc {
-        id: Singleton::PauseMenuDataMgr,
+    SingletonInfo::new(
+        Singleton::PauseMenuDataMgr,
         rel_start,
         size,
-        create,
-        ctor_invoke,
-    }
+        create_bytecode,
+    )
 }
